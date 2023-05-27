2b0a549d8eea888aff3dba3559b2d9fd3c5aa648
schmiedbarbara <schmiedbarbara1@gmail.com>
2023-05-27 13:18:16 -0300

    Agrego ejercicios resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,9 @@
+let a;
+let b;
+let c;
+let multiplicacion = (a*b* c);
+console.log(multiplicacion);
+
+a = 2;
+b = 17;
+c = 6;
---
+++ practico0/resta.js
@@ -0,0 +1,7 @@
+let minuendo;
+let sustraendo;
+let resta = minuendo - sustraendo;
+console.log(resta);
+
+minuendo = 10;
+sustraendo = 2;
