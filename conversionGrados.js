86714bf664af814f109fa64a3f914307810e9f36
schmiedbarbara <schmiedbarbara1@gmail.com>
2023-05-27 19:36:24 -0300

    Agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,11 @@
+let fahrenheit;
+let celsius= (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+fahrenheit = 50;
+celsius= (fahrenheit - 32) * 5/9;
+console.log("50 grados fahrenheit son",celsius, "grados celsius");
+
+fahrenheit = 75;
+celsius= (fahrenheit - 32) * 5/9;
+console.log("75 grados fahrenheit son",celsius, "grados celsius");
