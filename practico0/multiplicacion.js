1b17c41213c372fb3d18a4f21a82c8779e0e0115
schmiedbarbara <schmiedbarbara1@gmail.com>
2023-05-30 21:59:05 -0300

    Agrego practico0

--- practico0/multiplicacion.js
+++ practico0/multiplicacion.js
@@ -3,7 +3,3 @@ let b;
 let c;
 let multiplicacion = (a*b*c);
 console.log(multiplicacion);
-
-a = 2;
-b = 17;
-c = 6;
--- practico0/resta.js
+++ practico0/resta.js
@@ -2,6 +2,3 @@ let minuendo;
 let sustraendo;
 let resta = minuendo - sustraendo;
 console.log(resta);
-
-minuendo = 10;
-sustraendo= 2;
