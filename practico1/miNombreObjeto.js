c0bfbfc815e1aba81bb6957b007abd1b0619f933
schmiedbarbara <schmiedbarbara1@gmail.com>
2023-06-08 20:58:09 -0300

    Agregue ejercicio miNombreObjeto.js

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,5 @@
+//defini un objeto llamado persona, con los atributos nombre, apellido y edad
+let persona = {nombre: "Barbara", apellido: "Schmied", edad: 19};
+
+//imprimimos el objeto utilizando json.stringify para leer el contenido de este
+console.log("Mi objeto persona es el siguiente:", JSON.stringify(persona)); 
