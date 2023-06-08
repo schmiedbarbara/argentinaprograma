99182025009993a1299bbfbd989e4eb6366791fd
schmiedbarbara <schmiedbarbara1@gmail.com>
2023-06-08 20:56:42 -0300

    Agregue ejercicio miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,6 @@
+let nombre; //defini una variable nombre.
+nombre = "Barbara"; //guarde en la variable nombre mi nombre real.
+let apellido = "Schmied"; //defini la variable apellido y le asigné un valor en un mismo paso
+let edad = 19; //defini la variable edad con mi edad
+console.log("Hola, %s %s. Tienes %i años.", nombre, apellido, edad); 
+//imprimo resultados utilizando formatters 
